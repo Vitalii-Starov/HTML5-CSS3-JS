2b4e6e9d51dc3afedd81af8664ccd85c6259015f
Vitalii-Starov <vitalii_starov@mail.ru>
2017-05-23 13:25:53 +0300

    Add file

--- testjs.js
+++ testjs.js
@@ -8,9 +8,9 @@ function test2017(){
 
      if (document.tests.tab[0].checked && document.tests.tab[1].checked && document.tests.tab[2].checked && document.tests.tab[3].checked)
      {
-          alert("Неверный ввод!")}
+          alert("Возможно ввести только 2 варианта!")}
      else if(document.tests.tab[0].checked || document.tests.tab[3].checked){
-         alert("Неверный ввод!")
+         alert("Возможно ввести только 2 варианта!")
      }
      else if(document.tests.tab[1].checked  && document.tests.tab[2].checked)
          summa=summa+2;
@@ -20,9 +20,9 @@ function test2017(){
 
       if (document.tests.form[0].checked && document.tests.form[1].checked && document.tests.form[2].checked && document.tests.form[3].checked)
         {
-          alert("Неверный ввод!")}
+          alert("Возможно ввести только 2 варианта!")}
       else if(document.tests.form[2].checked || document.tests.form[3].checked){
-          alert("Неверный ввод!")
+          alert("Возможно ввести только 2 варианта!")
       }
       else if(document.tests.form[0].checked  && document.tests.form[1].checked)
           summa=summa+2;
