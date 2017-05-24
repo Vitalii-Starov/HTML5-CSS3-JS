6cb7a2c1dc52bea6808fb85dddce6707849d5ee4
Vitalii-Starov <vitalii_starov@mail.ru>
2017-05-24 17:20:53 +0300

    Add file Test

--- testjs.js
+++ testjs.js
@@ -6,24 +6,19 @@ function test2017(){
 
      if (document.tests.braus[3].checked){summa=summa+1;}
 
-     if (document.tests.tab[0].checked && document.tests.tab[1].checked && document.tests.tab[2].checked && document.tests.tab[3].checked)
-     {
+     if (document.tests.tab[0].checked && document.tests.tab[1].checked && document.tests.tab[2].checked && document.tests.tab[3].checked){
           alert("Выберите 2 варианта!")}
-     else if(document.tests.tab[0].checked || document.tests.tab[3].checked){
-         alert("Выберите 2 варианта!")
-     }
+     
      else if(document.tests.tab[1].checked  && document.tests.tab[2].checked)
          summa=summa+2;
      else if (document.tests.tab[1].checked || document.tests.tab[2].checked)
         summa=summa + 1;
 
 
-      if (document.tests.form[0].checked && document.tests.form[1].checked && document.tests.form[2].checked && document.tests.form[3].checked)
-        {
+      if (document.tests.form[0].checked && document.tests.form[1].checked && document.tests.form[2].checked && document.tests.form[3].checked){
           alert("Выберите 2 варианта!")}
-      else if(document.tests.form[2].checked || document.tests.form[3].checked){
-          alert("Выберите 2 варианта!")
-      }
+      
+
       else if(document.tests.form[0].checked  && document.tests.form[1].checked)
           summa=summa+2;
       else if (document.tests.form[0].checked || document.tests.form[1].checked)
